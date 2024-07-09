85079696fcfa355a0a6d5dacbbe04e25f1bb53f5
heal333 <rohanaich333@gmail.com>
2024-07-10 03:40:42 +0530

    updating .gitignore

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -4,22 +4,33 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 dotenv.config({ path: "./config.env" });
+
 const DB = process.env.DATABASE.replace(
   "<password>",
   process.env.DATABASE_PASSWORD
 );
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-mongoose.connect(DB).then((con) => {
-  //   console.log(con.connections);
-  console.log("succesful");
-});
-
 const port = 3000;
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    //   console.log(con.connections);
+    console.log("succesfully connected with the database");
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(
+      "failed to connect to the database or could error in port listing too idk or maybe the build in render failed"
+    );
+    console.log(err);
+  });
 
 // const userSchema = new mongoose.Schema({
 //   leaderBoard: Object,
@@ -30,27 +41,39 @@ const port = 3000;
 const leaderBoards = mongoose.model("leaderboards", new mongoose.Schema());
 const textStreams = mongoose.model("textstreams", new mongoose.Schema());
 
-app.get("/para", async (req, res) => {
-  const userData = await textStreams.find();
-  res.json(userData);
-});
-app.get("/top5", async (req, res) => {
-  const userData = await leaderBoards.find();
-
-  res.json(userData);
-});
-
 app.get("/", (req, res) => {
   res.status(200).json({ youGot: "rooted" });
 });
 
-app.put("/top5", async (req, res) => {
-  console.log(typeof req.body);
-  console.log("patched");
-  leaderBoards.collection.replaceOne({}, req.body);
-  res.status(200).json(req.body);
+app.get("/para", async (req, res) => {
+  try {
+    const userData = await textStreams.find();
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ error: "something is wrong with the server" });
+    console.log(error);
+  }
 });
 
-app.listen(port, () => {
-  console.log(`listing on port ${port}`);
+app.get("/top5", async (req, res) => {
+  try {
+    const userData = await leaderBoards.find();
+    res.status(200).json(userData);
+  } catch (error) {
+    res.status(500).json({ error: "internal server error" });
+    console.log(error);
+  }
+});
+
+app.put("/top5", async (req, res) => {
+  try {
+    // console.log(typeof req.body);
+    leaderBoards.collection.replaceOne({}, req.body);
+    res.status(200).json(req.body);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "internal server error, could not update leaderboards" });
+    console.log(error);
+  }
 });
